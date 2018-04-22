d6593c2f974b08577ea075a8b8a0d0e15fcf9042
Peter Reutemann <fracpete@gmail.com>
2018-04-22 21:31:04 +1200

    added execution via -e

--- src/main/java/com/github/fracpete/jrsync/Main.java
+++ src/main/java/com/github/fracpete/jrsync/Main.java
@@ -20,11 +20,91 @@
 
 package com.github.fracpete.jrsync;
 
+import com.github.fracpete.jrsync.Execute.ConsoleOutputListener;
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.impl.Arguments;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import net.sourceforge.argparse4j.inf.Namespace;
+import org.apache.commons.configuration2.INIConfiguration;
+
 /**
- * TODO: What class does.
+ * Starts up the user interface, if no parameters given.
  *
  * @author FracPete (fracpete at waikato dot ac dot nz)
  */
 public class Main {
 
+  /**
+   * Displays the user interface.
+   *
+   * @param session	the session to select initially, can be null or empty
+   */
+  public static void showGUI(String session) {
+    // TODO
+  }
+
+  /**
+   * Executes the specified session.
+   *
+   * @param session	the session to execute
+   * @throws Exception	if execution fails
+   */
+  public static void execute(String session) throws Exception {
+    String		msg;
+    INIConfiguration	config;
+    Execute		exec;
+
+    config = Configuration.read();
+    if (config == null)
+      throw new IllegalStateException("Failed to read configuration???");
+
+    exec = new Execute();
+    msg  = exec.execute(config, session, false, new ConsoleOutputListener());
+    if (msg != null)
+      throw new Exception(msg);
+  }
+
+  /**
+   * Executes the application.
+   *
+   * @param args	the command-line options
+   * @throws Exception	if parsing fails
+   */
+  public static void main(String[] args) throws Exception {
+    ArgumentParser 	parser;
+    Namespace 		ns;
+    String		session;
+
+    parser = ArgumentParsers.newArgumentParser(Main.class.getName());
+    parser.description("Frontend for rsync command-line execution.");
+    parser.addArgument("-e")
+      .action(Arguments.storeTrue())
+      .required(false)
+      .dest("execute")
+      .help("Automatically executes the specified session and exits.");
+    parser.addArgument("session")
+      .nargs("?")
+      .setDefault("")
+      .help("The session to use.");
+
+    ns = null;
+    try {
+      ns = parser.parseArgs(args);
+    }
+    catch (ArgumentParserException e) {
+      parser.handleError(e);
+    }
+    catch (Throwable t) {
+      throw t;
+    }
+
+    if (ns != null) {
+      session = ns.getString("session");
+      if (!session.isEmpty() && ns.getBoolean("execute"))
+	execute(session);
+      else
+	showGUI(session);
+    }
+  }
 }
